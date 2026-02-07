91916aa4548074d4a4edbe3270202d52b048ab8a
DSOE1024 <computerlub@163.com>
2026-02-08 03:49:07 +0800

    refactor: optimize actor system and add demo BGM

--- addons/konado/scripts/act/acting_Interface.gd
+++ addons/konado/scripts/act/acting_Interface.gd
@@ -211,7 +211,7 @@ func create_new_character(chara_id: String, division: int, pos: int, state: Stri
 	# 角色信息字典结构说明:
 	# {
 	#     "id": int,        # 角色唯一标识
-	#     "x": float,       # X轴坐标
+	#     "division": int,       # X轴坐标
 	#     "y": float,       # Y轴坐标
 	#     "state": String,   # 当前状态标识
 	#     "c_scale": float, # 缩放系数
@@ -237,16 +237,6 @@ func create_new_character(chara_id: String, division: int, pos: int, state: Stri
 	temp_node.set_character_texture(tex)
 	temp_node.set_texture_scale(actor_scale)
 	temp_node.mirror = mirror
-	# 创建角色的TextureRect
-	#var chara_tex = TextureRect.new()
-	# 先隐藏
-	#chara_tex.modulate = Color(1, 1, 1, 0)
-	#chara_tex.name = node_name
-	#chara_tex.set_texture(tex)
-	#chara_tex.scale = Vector2(_scale, _scale)
-	## 设置演员立绘水平镜像翻转，减少立绘文件资源占用
-	#chara_tex.flip_h = mirror
-	#temp_node.add_child(chara_tex)
 
 	# 添加到角色容器
 	_chara_controler.add_child(temp_node)
@@ -254,80 +244,19 @@ func create_new_character(chara_id: String, division: int, pos: int, state: Stri
 	character_created.emit()
 	print("在位置："+str(pos)+" 新建了演员："+str(chara_id)+" 演员状态："+str(state))
 	
-## 从角色字典新建角色
-func create_character_from_dic(_actor_dic: Dictionary) -> void:
-	actor_dict = _actor_dic
-	for chara in _actor_dic:
-		var chara_id = chara["id"]
-		var pos = Vector2(chara["x"], chara["y"])
-		var state = chara["state"]
-		var c_scale = chara["c_scale"]
-		var mirror = chara["mirror"]
-		
-		var node_name : String = chara_id
-		var temp_node : Node2D = Node2D.new()
-		temp_node.name = node_name
-		temp_node.set_position(pos)
-		# 创建角色的TextureRect
-		var chara_tex = TextureRect.new()
-		# 先隐藏
-		chara_tex.modulate = Color(1, 1, 1, 0)
-		chara_tex.name = node_name
 
-		var tex: Texture = Texture.new()
-		var target_chara: KND_Character = null
-
-		
-		for character in chara_list.characters:
-			if character.chara_name == chara_id:
-				target_chara = chara
-				break
-		
-		if target_chara == null:
-			print("目标角色为空")
-			continue
-			
-		# 读取对话的角色状态图片ID
-		var target_states = target_chara.chara_status
-
-		var target_state_tex
-		for character_state in target_states:
-			if state.status_name == state:
-				target_state_tex = state.status_texture
-				break
-
-		chara_tex.set_texture(tex)
-		chara_tex.scale = Vector2(c_scale, c_scale)
-		# 设置演员立绘水平镜像翻转，减少立绘文件资源占用
-		chara_tex.flip_h = mirror
-		temp_node.set_name(node_name)
-		temp_node.add_child(chara_tex)
-
-		# 添加到角色容器
-		_chara_controler.add_child(temp_node)
-		# _chara_controler.add_child(chara_tex)
-		
-		character_created.emit()
-		print("在位置："+str(pos)+" 新建了演员："+str(chara_id)+" 演员状态："+str(state))
-		
-	pass
-		
 # 切换演员的状态
 func change_actor_state(actor_id: String, state_id: String, state_tex: Texture) -> void:
-	var chara_node: Node = get_chara_node(actor_id)
+	var chara_node: KND_Actor = get_chara_node(actor_id)
 	if chara_node == null:
-		print("切换角色状态失败"+actor_id+"到"+str(state_tex))
-		return
-	var tex_node = chara_node.find_child(actor_id, true, false)
-	if tex_node:
-		# 修改字典中角色的状态
-		actor_dict[actor_id]["state"] = state_id
-		tex_node.set_texture(state_tex)
-		character_state_changed.emit()
-		print("切换"+actor_id+"到"+str(state_id)+"状态")
-	else:
-		character_state_changed.emit()
-		print("切换角色状态失败"+actor_id+"到"+str(state_tex))
+		printerr("切换角色状态失败"+actor_id+"到"+str(state_tex))
+		
+	# 修改字典中角色的状态
+	actor_dict[actor_id]["state"] = state_id
+	chara_node.set_character_texture(state_tex)
+	character_state_changed.emit()
+	print("切换"+actor_id+"到"+str(state_id)+"状态")
+
 
 # 高亮角色
 func highlight_actor(actor_id: String) -> void:
